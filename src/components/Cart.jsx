65bfaba89d653f45f9f5e75dc614cb6093826019
RAJAT SHARMA <56541315+Rajat-22@users.noreply.github.com>
2026-04-22 15:17:04 +0530

    Update Cart css if it's empty

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -25,18 +25,23 @@ export default function Cart(){
     open={userProgressCtx.progress === 'cart'} 
     onClose={userProgressCtx.progress === 'cart' ? handleHideCart : null}>
         <h2>Your Cart</h2>
-        <ul>
-            {cartCtx.items.map(item => 
-                <CartItem 
-                key={item.id} 
-                name={item.name}
-                quantity={item.quantity}
-                price={item.price}
-                onIncrease={() => cartCtx.addItem(item)}
-                onDecrease={() => cartCtx.removeItem(item.id)}
-                />
-            )}
-        </ul>
+        {cartCtx.items.length === 0 
+          ? <p className="cart-empty">Your cart is empty. Start adding some delicious meals! 🍽️</p>
+          : (
+            <ul>
+                {cartCtx.items.map(item => 
+                    <CartItem 
+                    key={item.id} 
+                    name={item.name}
+                    quantity={item.quantity}
+                    price={item.price}
+                    onIncrease={() => cartCtx.addItem(item)}
+                    onDecrease={() => cartCtx.removeItem(item.id)}
+                    />
+                )}
+            </ul>
+          )
+        }
         <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
         <p className="modal-actions">
             <Button textOnly onClick={handleHideCart}>Close</Button>
